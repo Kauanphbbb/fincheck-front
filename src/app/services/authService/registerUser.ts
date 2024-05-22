94af900084d295d1565453af98ab235de3294236
Kauanphbbb <kauanphbbb@gmail.com>
2024-05-22 18:07:25 -0300

    feat: register user

---
+++ src/app/services/authService/registerUser.ts
@@ -0,0 +1,22 @@
+import { sleep } from "../../utils/sleep";
+import { httpClient } from "../httpClient";
+
+export interface RegisterParams {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface RegisterResponse {
+  accessToken: string;
+}
+
+export async function registerUser(params: RegisterParams) {
+  await sleep(1000);
+  const { data } = await httpClient.post<RegisterResponse>(
+    "/auth/register",
+    params
+  );
+
+  return data;
+}
--- src/ui/pages/Register/useRegisterController.ts
+++ src/ui/pages/Register/useRegisterController.ts
@@ -1,6 +1,10 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { z } from "zod";
+import { authService } from "../../../app/services/authService";
+import { RegisterParams } from "../../../app/services/authService/registerUser";
 
 const schema = z.object({
   name: z.string().min(1, "Nome é obrigatório"),
@@ -19,13 +23,24 @@ export function useRegisterController() {
     resolver: zodResolver(schema),
   });
 
-  const handleSubmit = hookFormHandleSubmit((data) => {
-    console.log(data);
+  const { isPending, mutateAsync } = useMutation({
+    mutationFn: async (data: RegisterParams) => {
+      return authService.register(data);
+    },
+  });
+
+  const handleSubmit = hookFormHandleSubmit(async (data) => {
+    try {
+      const { accessToken } = await mutateAsync(data);
+    } catch {
+      toast.error("Ocorreu um erro ao criar a sua conta");
+    }
   });
 
   return {
     register,
     errors,
     handleSubmit,
+    isPending,
   };
 }
