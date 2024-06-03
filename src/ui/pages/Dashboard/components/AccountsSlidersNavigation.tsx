116f55c38ca4676b5d9f1e1e077a7538bdb61776
Kauanphbbb <kauanphbbb@gmail.com>
2024-06-03 16:51:00 -0300

    feat: disable slider options and responsive

--- src/ui/pages/Dashboard/components/AccountsSlidersNavigation.tsx
+++ src/ui/pages/Dashboard/components/AccountsSlidersNavigation.tsx
@@ -1,7 +1,15 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
 import { useSwiper } from "swiper/react";
 
-export function AccountsSlidersNavigation() {
+interface AccountsSlidersNavigationProps {
+  isBeginning: boolean;
+  isEnd: boolean;
+}
+
+export function AccountsSlidersNavigation({
+  isBeginning,
+  isEnd,
+}: AccountsSlidersNavigationProps) {
   const swiper = useSwiper();
 
   return (
@@ -9,12 +17,14 @@ export function AccountsSlidersNavigation() {
       <button
         onClick={() => swiper.slidePrev()}
         className="py-3 pl-2.5 pr-3.5 rounded-full enabled:hover:bg-black/10 transition-colors disabled:opacity-40"
+        disabled={isBeginning}
       >
         <ChevronLeftIcon className="text-white w-6 h-6" />
       </button>
       <button
         onClick={() => swiper.slideNext()}
         className="py-3 pl-2.5 pr-3.5 rounded-full enabled:hover:bg-black/10 transition-colors disabled:opacity-40"
+        disabled={isEnd}
       >
         <ChevronRightIcon className="text-white w-6 h-6" />
       </button>
