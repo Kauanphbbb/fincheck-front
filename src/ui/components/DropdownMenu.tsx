a10f218499a1ddb57b37cf94b99b841db98c407c
Kauanphbbb <kauanphbbb@gmail.com>
2024-06-05 18:05:48 -0300

    feat: create dropdown menu

---
+++ src/ui/components/DropdownMenu.tsx
@@ -0,0 +1,61 @@
+import * as RdxDropdownMenu from "@radix-ui/react-dropdown-menu";
+import React from "react";
+import { cn } from "../../app/utils/cn";
+
+interface DropdownMenuProps {
+  children: React.ReactNode;
+  className?: string;
+  onSelect?: () => void;
+}
+
+function DropdownMenuRoot({ children }: { children: React.ReactNode }) {
+  return <RdxDropdownMenu.Root>{children}</RdxDropdownMenu.Root>;
+}
+
+function DropdownMenuTrigger({ children }: { children: React.ReactNode }) {
+  return (
+    <RdxDropdownMenu.Trigger className="outline-none">
+      {children}
+    </RdxDropdownMenu.Trigger>
+  );
+}
+
+function DropdownMenuContent({ children, className }: DropdownMenuProps) {
+  return (
+    <RdxDropdownMenu.Portal>
+      <RdxDropdownMenu.Content
+        className={cn(
+          "rounded-2xl p-2 bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.1)] data-[side=bottom]:animate-slide-down-and-fade",
+          className
+        )}
+      >
+        {children}
+      </RdxDropdownMenu.Content>
+    </RdxDropdownMenu.Portal>
+  );
+}
+
+function DropdownMenuItem({
+  children,
+  className,
+  onSelect,
+}: DropdownMenuProps) {
+  return (
+    <RdxDropdownMenu.Item
+      onSelect={onSelect}
+      className={cn(
+        "min-h-12 outline-none flex items-center p-4 text-sm text-gray-800 rounded-2xl transition-colors cursor-pointer data-[highlighted]:bg-gray-50",
+        className
+      )}
+    >
+      {children}
+    </RdxDropdownMenu.Item>
+  );
+}
+
+export const DropdownMenu = {
+  Root: DropdownMenuRoot,
+  Trigger: DropdownMenuTrigger,
+  Content: DropdownMenuContent,
+  Item: DropdownMenuItem,
+};
--- src/ui/components/UserMenu.tsx
+++ src/ui/components/UserMenu.tsx
@@ -1,9 +1,28 @@
+import { ExitIcon } from "@radix-ui/react-icons";
+import { useAuth } from "../../app/hooks/useAuth";
+import { DropdownMenu } from "./DropdownMenu";
+
 export function UserMenu() {
+  const { logout } = useAuth();
+
   return (
-    <div className="bg-teal-100 rounded-full h-12 w-12 flex items-center justify-center">
-      <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">
-        KA
-      </span>
-    </div>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <div className="bg-teal-100 rounded-full h-12 w-12 flex items-center justify-center">
+          <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">
+            KA
+          </span>
+        </div>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content className="w-32">
+        <DropdownMenu.Item
+          onSelect={logout}
+          className="flex items-center justify-between"
+        >
+          Sair <ExitIcon className="w-4 h-4" />
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   );
 }
