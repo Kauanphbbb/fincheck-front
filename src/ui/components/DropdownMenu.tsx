89edc9e0e442387575a1436c2fb62ea2b04833c8
Kauanphbbb <kauanphbbb@gmail.com>
2024-06-06 16:54:20 -0300

    feat: create dropdown menu for transactions

--- src/ui/components/DropdownMenu.tsx
+++ src/ui/components/DropdownMenu.tsx
@@ -25,7 +25,7 @@ function DropdownMenuContent({ children, className }: DropdownMenuProps) {
     <RdxDropdownMenu.Portal>
       <RdxDropdownMenu.Content
         className={cn(
-          "rounded-2xl p-2 bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.1)] data-[side=bottom]:animate-slide-down-and-fade",
+          "rounded-2xl p-2 bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.1)] data-[side=bottom]:animate-slide-down-and-fade z-50",
           className
         )}
       >
@@ -44,7 +44,7 @@ function DropdownMenuItem({
     <RdxDropdownMenu.Item
       onSelect={onSelect}
       className={cn(
-        "min-h-12 outline-none flex items-center p-4 text-sm text-gray-800 rounded-2xl transition-colors cursor-pointer data-[highlighted]:bg-gray-50",
+        "min-h-10 outline-none flex items-center py-2 px-4 text-sm text-gray-800 rounded-2xl transition-colors cursor-pointer data-[highlighted]:bg-gray-50",
         className
       )}
     >
