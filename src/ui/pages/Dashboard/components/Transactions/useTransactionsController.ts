969641937da80faa5b320977a29bc53c8e7baa6b
Kauanphbbb <kauanphbbb@gmail.com>
2024-06-29 16:42:00 -0300

    feat: create filters modal

--- src/ui/pages/Dashboard/components/Transactions/useTransactionsController.ts
+++ src/ui/pages/Dashboard/components/Transactions/useTransactionsController.ts
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import { useDashboard } from "../DashboardContext/useDashBoard";
 
 export function useTransactionsController() {
   const { areValuesVisible } = useDashboard();
+
+  const [isFiltersModalOpen, setIsFiltersModalOpen] = useState(true);
+
+  function handleOpenFiltersModal() {
+    setIsFiltersModalOpen(true);
+  }
+
+  function handleCloseFiltersModal() {
+    setIsFiltersModalOpen(false);
+  }
+
   return {
     areValuesVisible,
     isInitialLoading: false,
     transactions: [],
     isLoading: false,
+    handleOpenFiltersModal,
+    handleCloseFiltersModal,
+    isFiltersModalOpen,
   };
 }
